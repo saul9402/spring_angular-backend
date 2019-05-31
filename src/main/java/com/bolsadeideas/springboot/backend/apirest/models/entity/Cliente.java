dd617b91f94d8a07b80f03548079ff712f21841f
Saul Avila <saul_harry_@hotmail.com>
2019-05-31 15:30:41 -0500

    se crea la entidad cliente y se agrega el acceso al base de datos en el yml

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,36 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String email;
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+}

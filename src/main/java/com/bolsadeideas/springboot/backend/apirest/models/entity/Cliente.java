95487aa0fa6f2ab14c1d5c9c435d2e77910582cd
Saul Avila <saul_harry_@hotmail.com>
2019-06-03 13:46:11 -0500

    se agregan mensajes customizados cuando hay error

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -36,16 +36,16 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotEmpty
-	@Size(min = 4, max = 12)
+	@NotEmpty(message = "No puede estar vacío")
+	@Size(min = 4, max = 12, message = "El tamaño tiene que estar entre 4 y 12 caracteres")
 	@Column(nullable = false)
 	private String nombre;
 
-	@NotEmpty
+	@NotEmpty(message = "No puede estar vacío")
 	private String apellido;
 
-	@NotEmpty
-	@Email
+	@NotEmpty(message = "No puede estar vacío")
+	@Email(message = "No es una direccion de correo bien formada")
 	@Column(nullable = false, unique = true)
 	private String email;
 

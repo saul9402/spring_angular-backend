3acbeb7fde46ff0ae1455ec474148b1dcaa778d9
Saul Avila <saul_harry_@hotmail.com>
2019-06-28 13:48:31 -0500

    se agregan dependencias de oauth2 y jwt. también se crean las entidades de usuario y rol con sus relaciones para manejar los inicios de sesión

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Usuario.java
@@ -0,0 +1,51 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, length = 20)
+	private String username;
+
+	@Column(length = 60)
+	private String password;
+
+	private boolean enabled;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "usuarios_roles",
+	joinColumns = @JoinColumn(name = "usuario_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"),
+	uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "role_id" })})
+	private List<Role> roles;
+
+}

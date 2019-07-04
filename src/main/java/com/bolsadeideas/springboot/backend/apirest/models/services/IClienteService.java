17e564975f967b6178350c78e0731b2baab5df8f
Saul Avila <saul_harry_@hotmail.com>
2019-07-04 11:07:39 -0500

    se agrega repositorio para facturas y se integra con el service clientes

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Factura;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Region;
 
 public interface IClienteService {
@@ -19,6 +20,13 @@ public interface IClienteService {
 	public Cliente save(Cliente cliente);
 
 	public void delete(Long id);
-	
+
 	public List<Region> findAllRegiones();
+
+	public Factura findFacturaById(Long id);
+
+	public Factura save(Factura factura);
+
+	public void deleteFacturaById(Long id);
+
 }
